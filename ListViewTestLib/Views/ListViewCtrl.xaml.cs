4150affedcc3ece265ffdbed6f6845a72307b71b
Kei Tashima <tassi.yuzukko@gmail.com>
2017-05-02 11:02:05 +0900

    参照設定を変更

--- ListViewTestLib/Views/ListViewCtrl.xaml.cs
+++ ListViewTestLib/Views/ListViewCtrl.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace ListViewTestLib
+namespace ListViewTestLib.Views
 {
     /// <summary>
     /// ListViewCtrl.xaml の相互作用ロジック
