36bb5bdd44b5a3cbb6933212f86ef1fea8856e4f
Kei Tashima <tassi.yuzukko@gmail.com>
2017-05-05 17:19:22 +0900

    リストビューの行色変更をStyleSelectorで実現しようとしたが、ボーダー処理が邪魔してできなかった

---
+++ ListViewTestLib/Views/StyleSelectors/ListViewItemStyleSelector.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace ListViewTestLib.Views.StyleSelectors
+{
+	public class ListViewItemStyleSelector : StyleSelector
+	{
+		public override Style SelectStyle(object item, DependencyObject container)
+		{
+			Style st = new Style();
+			st.TargetType = typeof(ListViewItem);
+			Setter backGroundSetter = new Setter();
+			backGroundSetter.Property = ListViewItem.BackgroundProperty;
+			ListView listView =
+				ItemsControl.ItemsControlFromItemContainer(container)
+				  as ListView;
+			int index =
+				listView.ItemContainerGenerator.IndexFromContainer(container);
+			if (index % 2 == 0)
+			{
+				backGroundSetter.Value = Brushes.LightBlue;
+			}
+			else
+			{
+				backGroundSetter.Value = Brushes.Beige;
+			}
+			st.Setters.Add(backGroundSetter);
+			return st;
+		}
+	}
+}
